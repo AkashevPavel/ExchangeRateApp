a3d600a3491b2323e37242a8bbe6773cb878dfcb
Pavel Akashev <pavela29@gmail.com>
2022-07-25 03:53:50 +0300

    feature/creating layouts
    Created layouts for mainact, host fragment, implemented viewPager

--- app/src/main/java/com/akashev_pavel/exchange_rate_app/MainActivity.kt
+++ app/src/main/java/com/akashev_pavel/exchange_rate_app/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.akashev_pavel.exchange_rate_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.akashev_pavel.exchange_rate_app.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class CurrencyActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
